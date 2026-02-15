b8e69b24fb7e9ccdd2dd28d3a1d4a524e3938a4f
Valen-tiina <vale.rodriguez.palacios@gmail.com>
2026-02-15 12:16:19 -0500

    credenciales bd en servidor

--- src/main/java/com/zabora/recipe_service/recipe_service/model/dtos/ingredientsdtos/ingredientDTO/CreateIngredient.java
+++ src/main/java/com/zabora/recipe_service/recipe_service/model/dtos/ingredientsdtos/ingredientDTO/CreateIngredient.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Size;
 
 public record CreateIngredient(
         @NotBlank(message = "El nombre del ingrediente es obligatorio")
-        @Size(min=5, max=100, message="El titulo debe tener entre 5 y 100 caracteres")
+        @Size(min=2, max=100, message="El titulo debe tener entre 5 y 100 caracteres")
         String name,
         String imageUrl
 ) {
