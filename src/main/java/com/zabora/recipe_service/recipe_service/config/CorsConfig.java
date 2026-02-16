808aed62582902ddeaa429dd25dae23e4584fbc1
Yulian4 <yulsyay19@gmail.com>
2026-02-15 23:29:16 -0500

    nuevo commit

--- src/main/java/com/zabora/recipe_service/recipe_service/config/CorsConfig.java
+++ src/main/java/com/zabora/recipe_service/recipe_service/config/CorsConfig.java
@@ -1,22 +1,22 @@
-package com.zabora.recipe_service.recipe_service.config;
+// package com.zabora.recipe_service.recipe_service.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig {
-     @Bean
-     public WebMvcConfigurer corsConfigurer() {
-         return new WebMvcConfigurer() {
-             @Override
-             public void addCorsMappings(CorsRegistry registry) {
-                 registry.addMapping("/**")
-                         .allowedOrigins("*")
-                         .allowedMethods("*")
-                         .allowedHeaders("*");
-             }
-         };
-     }
-}
+// @Configuration
+// public class CorsConfig {
+//      @Bean
+//      public WebMvcConfigurer corsConfigurer() {
+//          return new WebMvcConfigurer() {
+//              @Override
+//              public void addCorsMappings(CorsRegistry registry) {
+//                  registry.addMapping("/**")
+//                          .allowedOrigins("*")
+//                          .allowedMethods("*")
+//                          .allowedHeaders("*");
+//              }
+//          };
+//      }
+// }
